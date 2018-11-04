145d4be18fbafd2bed8679bd1f2694aa35335480
Robert Zablit <rzablit@gmail.com>
2018-11-03 19:17:51 -0700

    Initialize project
    This is the result of running the following command with Phoenix
    v1.4-rc.4:
    
    `mix phx.new --no-webpack --no-html --app scat_chat .`
    
    Nothing else has been modified.

---
+++ config/config.exs
@@ -0,0 +1,30 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :scat_chat,
+  ecto_repos: [ScatChat.Repo]
+
+# Configures the endpoint
+config :scat_chat, ScatChatWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "X838WxAdVuuBLjT6aLktvRKYlLCtTMe5cj0Kov1SnFkBWc83o9tFtOCA1SmTjy0B",
+  render_errors: [view: ScatChatWeb.ErrorView, accepts: ~w(json)],
+  pubsub: [name: ScatChat.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
