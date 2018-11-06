c2f7058d5ae079c7c06f53bd3ec72dccdc348dd3
Robert Zablit <rzablit@gmail.com>
2018-11-05 22:38:31 -0800

    Add travis-ci config
    We're specifying our Elixir and Erlang versions, postgresql, and
    environment preparation commands. We need to specify a custom travis
    mix configuration since the default postgres password for travis will be
    different from our local postgres password.
    
    We also whitelist the master branch as the only automatic branch
    trigger. Pull Requests are already automatically built, and this reduces
    unnecessary/duplicate builds.
    
    This also adds a badge reflecting the build status to the readme.

---
+++ config/travis.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :scat_chat, ScatChatWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :scat_chat, ScatChat.Repo,
+  username: "postgres",
+  password: "",
+  database: "scat_chat_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
