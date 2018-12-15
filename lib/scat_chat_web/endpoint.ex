2b8c375f34b1866c18177925a2b147f9ef1d4d63
Robert Zablit <rzablit@gmail.com>
2018-12-14 23:20:04 -0800

    Remove cookie configuration
    Cookies won't be relevant since we're not serving web pages.

--- lib/scat_chat_web/endpoint.ex
+++ lib/scat_chat_web/endpoint.ex
@@ -22,13 +22,5 @@ defmodule ScatChatWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_scat_chat_key",
-    signing_salt: "2xT9JFjl"
-
   plug ScatChatWeb.Router
 end
