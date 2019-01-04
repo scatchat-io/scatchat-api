d4402e08f8465dff855e80c9fbddccec896e1e62
Robert Zablit <rzablit@gmail.com>
2019-01-03 21:25:12 -0800

    Add timber plug integration
    No configuration necessary! Just added as a dependency.

--- mix.exs
+++ mix.exs
@@ -51,6 +51,7 @@ defmodule ScatChat.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:distillery, "~> 2.0"},
       {:timber, "~> 3.0"},
+      {:timber_plug, "~> 1.0"},
       {:excoveralls, "~> 0.10", only: :test},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
