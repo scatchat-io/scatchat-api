6a0c15e65d849e51c18142419cd68d8b9a9d517e
Robert Zablit <rzablit@gmail.com>
2019-01-09 21:56:25 -0800

    Add live reloading to phoenix
    This adds the `phoenix_live_reload` package and configures it in our
    endpoint. This builds upon the existing code reload configuration by
    automatically triggering a page refresh upon recompilation.
    
    Note: this will still be useful in development for GraphiQL even though
    we won't have any other pages being served by phoenix.

--- lib/scat_chat_web/endpoint.ex
+++ lib/scat_chat_web/endpoint.ex
@@ -8,6 +8,8 @@ defmodule ScatChatWeb.Endpoint do
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
   end
 
--- mix.exs
+++ mix.exs
@@ -47,6 +47,7 @@ defmodule ScatChat.MixProject do
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.4.0"},
       {:phoenix_ecto, "~> 4.0"},
+      {:phoenix_live_reload, "~> 1.2"},
       {:phoenix_pubsub, "~> 1.1"},
       {:plug_cowboy, "~> 2.0"},
       {:postgrex, ">= 0.0.0"},
