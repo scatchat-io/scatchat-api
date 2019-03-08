f12329483c9f666c6ba38339a7f9d9974b26ccf5
Robert Zablit <rzablit@gmail.com>
2019-03-08 00:38:33 -0800

    Add basic seed data for users

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias ScatChat.Repo
+
+alias ScatChat.Accounts.User
+
+Repo.insert!(%User{email: "phoenix@example.com"})
