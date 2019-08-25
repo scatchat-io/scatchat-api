d22db5955ccd9e64e9c54feef2abee37155dda35
Robert Zablit <rzablit@gmail.com>
2019-08-24 19:33:28 -0700

    Add types and resolver for users
    Our first absinthe code! We're exposing all current user fields in the
    type, and are defining a simple "list all" resolver. We've also added
    a simple resolver test.

---
+++ test/scat_chat_web/resolvers/accounts_test.exs
@@ -0,0 +1,14 @@
+defmodule ScatChatWeb.Resolvers.AccountsTest do
+  use ScatChat.DataCase
+
+  import ScatChat.Factory
+
+  alias ScatChatWeb.Resolvers.Accounts
+
+  describe "users" do
+    test "list_users/3 returns all users" do
+      insert(:user)
+      assert Accounts.list_users(nil, nil, nil) == {:ok, ScatChat.Accounts.list_users}
+    end
+  end
+end
